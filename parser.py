ebb308f1985eee3b8b4c267b42c685f2d20e0ade
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-11-01 23:08:07 +0100

    add bash test.sh

--- parser.py
+++ parser.py
@@ -1,12 +1,14 @@
+import sys
+
 import os
 from pathlib import Path
 import math
-import sys
 
 
 def create_dir(path: Path):
     if not os.path.exists(path):
         os.makedirs(Path(path))
+        print("Created dir", path)
 
 
 text = sys.stdin.readlines()
@@ -16,16 +18,21 @@ slot = None
 
 for element in text:
     element = element.rstrip()
-    if element == "inputCopy":
 
+    if counter == -1 and element != "inputCopy":
+        print("Wrong clipboard contents!")
+        exit(1)
+
+    if element == "inputCopy":
         test_case.append({"input": [], "output": []})
         slot = "input"
         counter += 1
-        continue
-    if element == "outputCopy":
+
+    elif element == "outputCopy":
         slot = "output"
-        continue
-    test_case[counter][slot].append(element)
+
+    else:
+        test_case[counter][slot].append(element)
 
 zfill = int(math.log10(len(test_case)))
 
@@ -36,6 +43,9 @@ for case_idx, case in enumerate(test_case, 1):
     create_dir(Path(dir_name))
 
     for key in ["input", "output"]:
-        with open(Path(dir_name, key), "w") as f:
+        filename = Path(dir_name, f"{key}.txt")
+        with open(filename, "w") as f:
             text = os.linesep.join(case[key])
             f.write(text)
+            f.write("\n")
+        print("Created file", str(filename))
