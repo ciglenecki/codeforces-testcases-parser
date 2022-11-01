fc2c29b30951d55c549da0d13ffdbbb18204d35b
Matej Ciglenečki <matej.ciglenecki@gmail.com>
2022-11-02 00:53:46 +0100

    functions to py

--- create_tests.py
+++ create_tests.py
@@ -11,41 +11,50 @@ def create_dir(path: Path):
         print("Created dir", path)
 
 
-text = sys.stdin.readlines()
-test_case = []
-counter = -1
-slot = None
+def parse_clipboard() -> list[dict[str, list[str]]]:
 
-for element in text:
-    element = element.rstrip()
+    text = sys.stdin.readlines()
+    test_cases = []
+    counter = -1
+    slot = None
 
-    if counter == -1 and element != "inputCopy":
-        print("Wrong clipboard contents!")
-        exit(1)
+    for element in text:
+        element = element.rstrip()
 
-    if element == "inputCopy":
-        test_case.append({"input": [], "output": []})
-        slot = "input"
-        counter += 1
+        if counter == -1 and element != "inputCopy":
+            print("Wrong clipboard contents!")
+            exit(1)
 
-    elif element == "outputCopy":
-        slot = "output"
+        if element == "inputCopy":
+            test_cases.append({"input": [], "output": []})
+            slot = "input"
+            counter += 1
 
-    else:
-        test_case[counter][slot].append(element)
+        elif element == "outputCopy":
+            slot = "output"
 
-zfill = int(math.log10(len(test_case)))
+        else:
+            test_cases[counter][slot].append(element)
+    return test_cases
 
-for case_idx, case in enumerate(test_case, 1):
-    test_number = str(case_idx).zfill(zfill)
-    dir_name = f"test_{test_number}"
 
-    create_dir(Path(dir_name))
+def create_test_files(test_cases, zfill):
+    for i, test_case in enumerate(test_cases, 1):
+        test_case_number = str(i).zfill(zfill)
+        dir_name = f"test_{test_case_number}"
 
-    for key in ["input", "output"]:
-        filename = Path(dir_name, f"{key}.txt")
-        with open(filename, "w") as f:
-            text = os.linesep.join(case[key])
-            f.write(text)
-            f.write("\n")
-        print("Created file", str(filename))
+        create_dir(Path(dir_name))
+
+        for key in ["input", "output"]:
+            filename = Path(dir_name, f"{key}.txt")
+            with open(filename, "w") as f:
+                text = os.linesep.join(test_case[key])
+                f.write(text)
+                f.write("\n")
+            print("Created file", str(filename))
+
+
+if __name__ == "__main__":
+    test_cases = parse_clipboard()
+    zfill = int(math.log10(len(test_cases)))
+    create_test_files(test_cases, zfill)
